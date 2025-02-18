02ea0a04a5b07e9b881a13ef7a7d1f47199b83c7
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2025-02-18 09:37:22 +0100

    deploying online

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
+// client/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Echo_Social/',  // <--- aggiunto
   plugins: [react()],
   server: {
     port: 5173
