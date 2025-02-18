701b862e263be3ce6ac6a4c7ed4803c63d12c735
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2025-02-18 11:10:52 +0100

    try guillame

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Echo_Social/',  // <--- aggiunto
+  // base: '/Echo_Social/',  // <--- aggiunto
   plugins: [react()],
   server: {
     port: 5173
